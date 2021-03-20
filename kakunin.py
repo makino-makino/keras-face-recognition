63315301f984209520bd873f52cf801f246e7317
kino-ma <kino.ma.ms@gmail.com>
2021-03-21 08:16:07 +0900

    devide client code

--- kakunin.py
+++ kakunin.py
@@ -18,45 +18,65 @@ from saas.model_client.service.model_client_pb2_grpc import ModelClientStub
 from saas.model_client.service.model_client_pb2 import ModelInfo, ModelBinaryKeras
 
 model_file = './data/model/model.h5'
-model = model = load_model(model_file)
+model = load_model(model_file)
 
-test_images_tmpl = './data/divide/{}/b/*.jpg'
+test_images_tmpl = './data/devide/{}/test/*.jpg'
 
 murai_correct = 0
 murai_score = []
 
-host = "yahoo-hackathon-gateai-1.japaneast.cloudapp.azure.com"
-input_client_port = 5001
-model_client_port = 5002
 
-# grpc stub to input_client
-channel_input_client = grpc.insecure_channel(
-    f'{host}:{str(input_client_port)}', options=[])
-api_input_client = InputClientStub(channel_input_client)
+def test_client(test_data):
+    host = "yahoo-hackathon-gateai-1.japaneast.cloudapp.azure.com"
+    input_client_port = 5001
+    model_client_port = 5002
 
-# grpc stub to model_client
-channel_model_client = grpc.insecure_channel(
-    f'{host}:{str(model_client_port)}', options=[])
-api_model_client = ModelClientStub(channel_model_client)
 
-# call inpuot_client to generate keys
-print("call gen_key")
-api_input_client.gen_key(NoParam())
+    ## サーバ側
 
-"""
-# call model_client to upload h5 file from modelfiles/h5/{model_name}.h5
-print("call load_and_compile_model_from_local_h5")
-model_info = ModelBinaryKeras()
-model_info.config = pickle.dumps(model.get_config())
-model_info.weights = pickle.dumps(model.get_weights())
-model_info.type_info = 'sequential'
-model_info.intermediate_output = 'none'
-api_model_client.compile_model_from_binary_keras(model_info)
-"""
+    # grpc stub to model_client
+    channel_model_client = grpc.insecure_channel(
+        f'{host}:{str(model_client_port)}', options=[])
+    api_model_client = ModelClientStub(channel_model_client)
+
+    # モデルのアップロード+暗号化
+    # サーバが落ちてる？
+    # call model_client to upload h5 file from modelfiles/h5/{model_name}.h5
+    print("call load_and_compile_model_from_local_h5")
+    model_info = ModelBinaryKeras()
+    model_info.config = pickle.dumps(model.get_config())
+    model_info.weights = pickle.dumps(model.get_weights())
+    model_info.type_info = 'sequential'
+    model_info.intermediate_output = 'none'
+    # ここで止まる
+    #api_model_client.compile_model_from_binary_keras(model_info)
+
+
+    ## クライアント側
+
+    # grpc stub to input_client
+    channel_input_client = grpc.insecure_channel(
+        f'{host}:{str(input_client_port)}', options=[])
+    api_input_client = InputClientStub(channel_input_client)
+
+    # call inpuot_client to generate keys
+    print("call gen_key")
+    #ここはうごく
+    api_input_client.gen_key(NoParam())
+
+
+    data_for_cf = test_data.tolist()
+    input_data = Tensor()
+    input_data.data = pickle.dumps(data_for_cf)
+
+    # call prediction
+    # ここも止まる
+    result = api_input_client.predict(input_data)
+    result = pickle.loads(result.data)
+
 
 
 murai_test_images = glob.glob(test_images_tmpl.format("murai"))
-print(murai_test_images)
 
 for img_path in murai_test_images:
     img = img_to_array(load_img(img_path, target_size=(50, 50)))
@@ -87,6 +107,7 @@ for img_path in human_test_images:
     img_nad = img_nad[None, ...]
 
     label = ['human', 'murai']
+    # かえる
     pred = model.predict(img_nad, batch_size=1, verbose=0)
     score = np.max(pred)
     pred_label = label[np.argmax(pred[0])]
@@ -108,3 +129,12 @@ print()
 print(f"human correct: {human_correct} / {human_test_count}")
 print(f"human accuracy: {human_correct / human_test_count * 100}%")
 print("human score average:", np.mean(human_score))
+
+
+print("start client test")
+client_test_image = murai_test_images[0]
+img = img_to_array(load_img(client_test_image, target_size=(50, 50)))
+img_nad = img_to_array(img)/255
+img_nad = img_nad[None, ...]
+client_test_data = img_nad
+test_client(client_test_data)
